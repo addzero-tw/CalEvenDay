4bd84418606c6f7129b60d37f854f27239c7b371
addzero-tw <addzero-tw@umail.hinet.net>
2016-08-15 19:12:21 +0800

    no message

--- datas/Buddha.js
+++ datas/Buddha.js
@@ -1,9 +1,20 @@
+var parse = require('csv-parse');
+require('should');
 
+var inputFile='name_list.csv';
 
 exports.QueryName = function(Name,cb) {
     if (typeof cb !== 'function') {
         throw new Error('need a callback');
     }
-	
-	cb(err, result);
+	var parser = parse({delimiter: ','}, function (err, data) {
+		var result = new Array();
+		for(var i=1;i<data.length;i++){
+			var onevals = data[i];
+			if(onevals[1].indexOf(Name) > -1) 
+					result.push({ID:onevals[0],Name:onevals[1],Note:onevals[2]});
+		}
+		cb(err, result);
+	});
+	fs.createReadStream(inputFile).pipe(parser);
 }
