534edf898971f5a998e345e92100e3ff18cacbec
addzero-tw <addzero-tw@umail.hinet.net>
2016-08-18 19:17:25 +0800

    no message

--- public/javascripts/CountOff.js
+++ public/javascripts/CountOff.js
@@ -1,14 +1,29 @@
 function SearchByID() {
-	alert('SearchByID');
+	//alert('SearchByID');
+	var Id = $('#IDTb').val();
+	
+	
+	if(Id.length == 1) {
+		$.getJSON({url:'./QueryID?ID='+Id,function(data){
+			$('#IDList').show().empty()
+			$.each( data, function( ID, Name,Note ) {
+				//items.push( "<li id='" + ID + "' Name='"+Name+"' Note='"+Note+"'>" + ID + "</li>" );
+			});
+			
+			
+		}})
+	}
 }
 function SearchByName() {
-	alert('SearchByName');
+	//alert('SearchByName');
 }
 
 function init() {
 	//alert("init");
 	$('#IDTb').change(SearchByID);
 	$('#NamtTb').change(SearchByName);
+	$('#IDList').hidden();
+	$('#NameList').hidden();
 }
 
 
