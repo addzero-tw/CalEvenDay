f407e7375f92b50a0be04199e26a5df75edc1081
Addzero <addzero.lee@gmail.com>
2016-08-18 22:41:25 +0800

    no message

--- public/javascripts/CountOff.js
+++ public/javascripts/CountOff.js
@@ -2,14 +2,22 @@ function SearchByID() {
 	//alert('SearchByID');
 	var Id = $('#IDTb').val();
 	
-	
 	if(Id.length == 1) {
 		$.getJSON({url:'./QueryID?ID='+Id,function(data){
-			$('#IDList').show().empty()
+			$('#IDList').show().empty();
+			alert(data);
 			$.each( data, function( ID, Name,Note ) {
 				//items.push( "<li id='" + ID + "' Name='"+Name+"' Note='"+Note+"'>" + ID + "</li>" );
+				var li = $('<li>'+ID+'</li>');
+				li.attr({
+					"id":ID
+					,"Name":Name
+					,"Note":Note
+				});
+				$('#IDList').append(li);
 			});
-			
+			var count = $("#IDList li").length;
+			alert(count);
 			
 		}})
 	}
