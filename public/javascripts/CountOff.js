3cba99aeb66679eb4dc2bca25015085ef785c033
addzero-tw <addzero-tw@umail.hinet.net>
2016-08-18 18:36:35 +0800

    no message

--- public/javascripts/CountOff.js
+++ public/javascripts/CountOff.js
@@ -1,1 +1,19 @@
-alert(1);
+function SearchByID() {
+	alert('SearchByID');
+}
+function SearchByName() {
+	alert('SearchByName');
+}
+
+function init() {
+	//alert("init");
+	$('#IDTb').change(SearchByID);
+	$('#NamtTb').change(SearchByName);
+}
+
+
+
+
+
+
+$(document).ready(init);
